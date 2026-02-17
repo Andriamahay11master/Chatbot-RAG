162ba2a05182336c2a229c560c38da14685ee2f7
Mahay <hirimanana@yahoo.fr>
2026-02-17 20:53:26 +0400

    add embeddings back

---
+++ back/embeddings.py
@@ -0,0 +1,17 @@
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+# Load embedding model
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def create_vector_index(chunks):
+    texts = [chunk["text"] for chunk in chunks]
+
+    embeddings = embedding_model.encode(texts, convert_to_numpy=True)
+
+    dimension = embeddings.shape[1]
+    index = faiss.IndexFlatL2(dimension)
+    index.add(embeddings)
+
+    return index, embeddings
