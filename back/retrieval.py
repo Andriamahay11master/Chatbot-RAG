7a21e6b8ad3916dbe309857c6e11b6753d5fb1a0
Mahay <hirimanana@yahoo.fr>
2026-02-17 21:00:47 +0400

    add retrieval function to back

---
+++ back/retrieval.py
@@ -0,0 +1,9 @@
+def retrieve(query, index, chunks, top_k=3):
+    query_embedding = embedding_model.encode([query], convert_to_numpy=True)
+    distances, indices = index.search(query_embedding, top_k)
+
+    retrieved_chunks = []
+    for idx in indices[0]:
+        retrieved_chunks.append(chunks[idx])
+
+    return retrieved_chunks
