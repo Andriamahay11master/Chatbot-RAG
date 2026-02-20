d66d76ab4c2a45b97417ebb6b964fca8176da821
Mahay <hirimanana@yahoo.fr>
2026-02-21 00:01:24 +0400

    add pipeline run models

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Load your regulation text
 from back.embeddings import create_vector_index
 from back.preprocessing import clause_aware_chunking, clean_text, extract_pdf_text
+from back.generative import evaluate_model
 
 
 pdf_path = "/data/RCentre.pdf"  # Your PDF file
@@ -11,3 +12,13 @@ cleaned_text = clean_text(raw_text)
 chunks = clause_aware_chunking(cleaned_text)
 
 index, embeddings = create_vector_index(chunks)
+
+#Pipeline execution for three models
+results_qwen = evaluate_model("Qwen/Qwen2.5-7B-Instruct")
+print(results_qwen)
+
+results_mistral = evaluate_model("mistralai/Mistral-7B-Instruct-v0.2")
+print(results_mistral)
+
+results_phi = evaluate_model("microsoft/Phi-3.5-mini-instruct")
+print(results_phi)
